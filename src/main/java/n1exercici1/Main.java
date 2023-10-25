a4450dbbe6cf58529cbce5323eb463a8f2ccc7eb
JoseRodriguezCubero <josanrocu@gmail.com>
2023-10-25 02:12:50 +0200

    segunda revision

--- src/main/java/n1exercici1/Main.java
+++ src/main/java/n1exercici1/Main.java
@@ -3,7 +3,6 @@ package n1exercici1;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.function.Predicate;
 
 public class Main {
